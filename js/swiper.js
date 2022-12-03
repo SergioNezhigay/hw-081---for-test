e4d0b0cd8de7a7a356dc01398de1624a49b955c4
Sergey Nezhigay <abc1971h@gmail.com>
2022-12-04 01:17:17 +0200

    tablet+desktop

--- js/swiper.js
+++ js/swiper.js
@@ -9,4 +9,16 @@ var swiper = new Swiper('.mySwiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    // when window width is >= 480px
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
 });
